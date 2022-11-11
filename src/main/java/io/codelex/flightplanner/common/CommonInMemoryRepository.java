d6a7f40d83168758a4bd216e1135d6f6cafed6a0
Zigmārs Strods <109984075+zigmarsstrods@users.noreply.github.com>
2022-11-11 10:51:23 +0200

    Simplified MVC model where all data storage is done in repository class

--- src/main/java/io/codelex/flightplanner/common/CommonInMemoryRepository.java
+++ src/main/java/io/codelex/flightplanner/common/CommonInMemoryRepository.java
@@ -2,44 +2,82 @@ package io.codelex.flightplanner.common;
 
 import io.codelex.flightplanner.domain.*;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Set;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 @Repository
 @ConditionalOnProperty(prefix = "flight-planner", name = "store-type", havingValue = "in-memory")
 public class CommonInMemoryRepository implements CommonRepository {
 
-    private final MemoryDB memoryDB;
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    private final List<Flight> flightList = new ArrayList<>();
 
-    public CommonInMemoryRepository(MemoryDB memoryDB) {
-        this.memoryDB = memoryDB;
+    public CommonInMemoryRepository() {
     }
 
     public Flight addFlight(AddFlightRequest flightRequest) {
-        return memoryDB.addFlight(flightRequest);
+        int flightId = flightList.size();
+        Airport flightFrom = flightRequest.getFrom();
+        Airport flightTo = flightRequest.getTo();
+        String flightCarrier = flightRequest.getCarrier();
+        LocalDateTime flightDepartureTime = LocalDateTime.parse(flightRequest.getDepartureTime(), formatter);
+        LocalDateTime flightArrivalTime = LocalDateTime.parse(flightRequest.getArrivalTime(), formatter);
+        if (flightDepartureTime.compareTo(flightArrivalTime) >= 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Entered times not possible for a correct flight");
+        }
+        Flight flightToAdd = new Flight(flightId, flightFrom, flightTo, flightCarrier, flightDepartureTime, flightArrivalTime);
+        if (flightList.stream().anyMatch(flight -> flight.equals(flightToAdd))) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "Can not add 2 identical flights!");
+        }
+        flightList.add(flightToAdd);
+        return flightToAdd;
     }
 
-    @Override
     public Flight fetchFlight(int id) {
-        return memoryDB.fetchFlight(id);
+        return flightList.stream().
+                filter(flight -> flight.getId() == id)
+                .findAny()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Requested flight not found in the DB!!!"));
     }
 
     public PageResult<Flight> searchFlights(SearchFlightsRequest request) {
-        return memoryDB.searchFlights(request);
+        List<Flight> flightsFound = flightList.stream()
+                .filter(flight -> flight.getFrom().getAirport().equals(request.getFrom()) &&
+                        flight.getTo().getAirport().equals(request.getTo()) &&
+                        flight.getDepartureTime().toLocalDate().toString().equals(request.getDepartureDate())).toList();
+        return new PageResult<>(0, flightsFound.size(), flightsFound);
     }
 
     public Set<Airport> searchAirports(String search) {
-        return memoryDB.searchAirports(search);
+        Set<Airport> matchedAirport = new HashSet<>();
+        String normalizedSearch = search.trim()
+                .toLowerCase();
+        Optional<Flight> matchedFromAirport = flightList.stream()
+                .filter(flight -> flight.getFrom().getCountry().toLowerCase().startsWith(normalizedSearch) ||
+                        flight.getFrom().getCity().toLowerCase().startsWith(normalizedSearch) ||
+                        flight.getFrom().getAirport().toLowerCase().startsWith(normalizedSearch))
+                .findAny();
+        Optional<Flight> matchedToAirport = flightList.stream()
+                .filter(flight -> flight.getTo().getCountry().toLowerCase().startsWith(normalizedSearch) ||
+                        flight.getTo().getCity().toLowerCase().startsWith(normalizedSearch) ||
+                        flight.getTo().getAirport().toLowerCase().startsWith(normalizedSearch))
+                .findAny();
+        matchedFromAirport.ifPresent(flight -> matchedAirport.add(flight.getFrom()));
+        matchedToAirport.ifPresent(flight -> matchedAirport.add(flight.getTo()));
+        return matchedAirport;
     }
 
-    @Override
     public void deleteFlight(int id) {
-        memoryDB.deleteFlight(id);
+        flightList.removeIf(flight -> flight.getId() == id);
     }
 
     public void clear() {
-        memoryDB.clear();
+        flightList.clear();
     }
 
 }
